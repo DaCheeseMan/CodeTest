b26d589310f4df651625d392adabe1ecce680075
DaCheeseMan <andreas.fransson@gmail.com>
2014-12-29 14:17:27 +0100

    First commit

---
+++ CodeTest.Web/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace CodeTest.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
