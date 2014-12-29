f22bf8919c46f31428a18a2e784722e66411e68f
DaCheeseMan <andreas.fransson@gmail.com>
2014-12-29 23:04:46 +0100

    Added send message code to console project

--- CodeTest.Console/Program.cs
+++ CodeTest.Console/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using CodeTest.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,39 @@ namespace CodeTest.Console
     {
         static void Main(string[] args)
         {
+        }
+
+        private void SendMessage()
+        {            
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:56851/");
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            
+            var message = new Message()
+                {
+                    Text = "Hello World!",
+                    Owner = new Person()
+                    {  
+                      FirstName = "Andreas",
+                      LastName = "Fransson",
+                      Email = "andreas.fransson@gmail.com"
+                    },
+                    DateCreated = DateTime.Now
+                };
+            var parameter = JsonConvert.SerializeObject(message);
+            HttpContent content = new StringContent(parameter, Encoding.UTF8, "application/json");
+            
+            var response = client.PostAsync("api/User", content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                System.Console.WriteLine("Message sent! Please refresh your browser.");                
+            }
+            else
+            {
+                System.Console.WriteLine("An error ocurred. Status code: " + response.StatusCode + " - Message: " + response.ReasonPhrase);
+            }
         }
     }
 }
